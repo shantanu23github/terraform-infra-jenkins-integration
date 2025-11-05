6e03676d128efef8dc22e0d50be1d22f90f0e0ca
Shantanu Mane <46455521+shantanu23github@users.noreply.github.com>
2025-11-06 03:58:16 +0530

    Create main.tf

---
+++ modules/alb/main.tf
@@ -0,0 +1,36 @@
+resource "aws_lb" "this" {
+  name               = "${var.name}"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.public_subnet_ids
+  security_groups    = [var.alb_security_group_id]
+  tags = merge(var.tags, { Name = "${var.name}" })
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name        = "${var.name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+  health_check {
+    path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+  tags = merge(var.tags, { Name = "${var.name}-tg" })
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
