8e08eccf5e07c0c0d6b32e52da17af98d2a8d4fa
Shantanu Mane <46455521+shantanu23github@users.noreply.github.com>
2025-11-06 12:24:31 +0530

    Delete envs/dev directory

--- envs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "myapp-tf-state-s3-backend-bucket"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-locks"
-    encrypt        = true
-  }
-}
