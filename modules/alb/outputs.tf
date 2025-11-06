04963ef89ca10ab2f7bdab5de731dd3cdd7e12ba
Shantanu Mane <46455521+shantanu23github@users.noreply.github.com>
2025-11-06 12:24:39 +0530

    Delete modules directory

--- modules/alb/outputs.tf
+++
@@ -1,4 +0,0 @@
-
-output "alb_arn" { value = aws_lb.this.arn }
-output "alb_dns" { value = aws_lb.this.dns_name }
-output "target_group_arn" { value = aws_lb_target_group.app_tg.arn }
--- modules/asg/outputs.tf
+++
@@ -1,2 +0,0 @@
-output "asg_name" { value = aws_autoscaling_group.asg.name }
-
